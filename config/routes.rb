4d01e8857bdcfb3d3971b79d2bac769ec5371601
Christophe Cholot <christophe.cholot@gmail.com>
2022-11-15 02:15:59 +0100

    [Routes] remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :articles do 
-    get :list, on: :collection
-  end
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
